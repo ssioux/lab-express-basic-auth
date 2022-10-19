802bfdf0f7c0c47683c89765bce65eefd4d811e5
David Lazaro <dlazaro7@gmail.com>
2022-10-19 23:49:16 +0200

    Bcryptjs added in signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { findById } = require("../models/User.model.js");
 const User = require("../models/User.model.js");
+const bcrypt = require("bcryptjs");
 
 // GET "/auth/signup" => Renderiza Formulario con registro de usuario
 router.get("/signup", (req, res, next) => {
@@ -13,40 +13,45 @@ router.post("/signup", async (req, res, next) => {
   console.log(username);
 
   try {
-    const userInBD = await User.findOne({username: username})
-    console.log(userInBD)
+    const userInBD = await User.findOne({ username: username });
+    console.log(userInBD);
 
+    if (userInBD !== null) {
+      res.render("auth/signup.hbs", {
+        messageError: "This User is used, choose another",
+      });
+    
+      return;
+    }
 
+    
+    const salt = await bcrypt.genSalt(10)
+    const hashPass = await bcrypt.hash(password, salt)
 
+    // const hashPass = bcrypt.hash(password, 12); // segundo argumento el salt
 
-    // await User.create({
-    //   username: username,
-    //   password: password,
-    // });
-  
+    await User.create({
+      username: username,
+      password: hashPass
+    });
+
     res.redirect("/auth/login");
-
   } catch (error) {
-    next(error)
+    next(error);
   }
-
-
-  
 });
 
 // GET "/auth/login" => Renderiza Formulario para acceso perfil.
-router.get("/login", (req,res,next) => {
-
-    res.render("auth/login.hbs")
-})
+router.get("/login", (req, res, next) => {
+  res.render("auth/login.hbs");
+});
 
 // POST "/auth/login" => Acceso Perfil(Recoge info de hbs yaccede al perfil)
-router.post("/login", async(req, res, next)=>{
-    const {username, password} = req.body
-    console.log(req.body)
-    // await User.findById()
-    res.redirect("/")
-
-})
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+  console.log(req.body);
+  // await User.findById()
+  res.redirect("/");
+});
 
 module.exports = router;
